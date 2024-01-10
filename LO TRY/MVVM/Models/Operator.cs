aa7ccc4ba6b1b8512e4fa4da2554c4e779484f9f
hugo.soeurpro <hugo.soeurpro@gmail.com>
2024-01-10 11:11:44 +0100

    Add project files.

---
+++ LO TRY/MVVM/Models/Operator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LO_TRY.MVVM.Models
+{
+    public class Operator
+    {
+    }
+}
---
+++ TEST LO TRY/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TEST_LO_TRY
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
